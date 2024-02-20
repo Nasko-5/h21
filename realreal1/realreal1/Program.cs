26dede5f9d3d6d3e2fe83ce8d358c26f048eab5e
Arco-IT <159255114+Arco-IT@users.noreply.github.com>
2024-02-21 00:41:22 +0200

    Add files via upload
    Nasko ti si

---
+++ realreal1/realreal1/Program.cs
@@ -0,0 +1,9 @@
+﻿double popravi = double.Parse(Console.ReadLine());
+double tezi = double.Parse(Console.ReadLine());
+double promenlivi = 1;
+for (double i = 0; i < tezi; i++)
+{
+    promenlivi = promenlivi * popravi;
+}
+Console.WriteLine(promenlivi);
+//Tva primerno mojesh da go mahnesh i da napishesh kato komentar *mahnah nenujen tekst* or something idk!?
